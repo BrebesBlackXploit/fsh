39103dfd55cca1e1d193f3ef53412b633b8e87d3
BrebesBlackXploit <rerelasy5@gmail.com>
2022-09-29 15:14:18 +0700

    Script Auto Installer ssh

---
+++ ssh/about.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#  Premium Auto Script Created By RamaGans ID   #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By RamaGans ID                   #"
+echo -e "================================================="
+echo -e "# Thanks To                                     #"
+echo -e "================================================="
+echo -e "# Allah SWT                                     #"
+echo -e "# My Family                                     #"
+echo -e "# Brebes Black Xploit                           #"
+echo -e "# SL                                            #"
+echo -e "# LamVpn                                        #"
+echo -e "# Horasss                                       #"
+echo -e "================================================="
